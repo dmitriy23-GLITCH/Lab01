6697d5e3abad85319236234df07c9cd8aba0db1e
dmitriy23-GLITCH <rezermat296@gmail.com>
2026-09-06 21:56:22 +0300

    lab01laboratorkadubinin

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -1,6 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("ВИЗИТКА СИСТЕМЫ");
+Console.ResetColor();
+
+string studentName = "Дмитрий";
+string studentGroup = "ПМБИ-261";
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("\n[Студент]");
+Console.ResetColor();
+Console.WriteLine($"Имя :   {studentName}");
+Console.WriteLine($"Группа :   {studentGroup}");
+Console.WriteLine($"Дата :   {DateTime.Now:dd.MM.yyyy HH:mm}");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("\n[Компьютер]");
+Console.ResetColor();
+Console.WriteLine($"Имя машины:    {Environment.MachineName}");
+Console.WriteLine($"Пользователь:  {Environment.UserName}");
+Console.WriteLine($"OC:            {Environment.OSVersion}");
+Console.WriteLine($"64-битная OC:  {Environment.Is64BitOperatingSystem}");
+Console.WriteLine($"Версия .NET:   {Environment.Version}");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("\n[Процессор]");
+Console.ResetColor();
+Console.WriteLine($"Логических ядер: {Environment.ProcessorCount}");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("\n[Память]");
+Console.ResetColor();
+long workingSetMb = Environment.WorkingSet / 1024 / 1024;
+Console.WriteLine($"Память процессора: {workingSetMb} МБ");
+Console.WriteLine($"Размер указателя : {IntPtr.Size * 8} бит");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nПрограмма выполнена успешно!");
+Console.ResetColor();
